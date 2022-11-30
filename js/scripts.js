216194377defb6b3a37c260544b61ffd73c7a6ab
patriciacostadacruz <patriciacstcz@gmail.com>
2022-11-30 19:18:11 +0100

    Enables try again button to replay when user lost

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,18 @@ window.onload = function () {
   const ctx = canvas.getContext('2d');
   const startPage = document.getElementById('start-page');
   const startButton = document.getElementById('start');
-  
+  const replayButton = document.getElementById("try-again");
+  const loosePage = document.getElementById("lose-page");
+
   startButton.onclick = function () {
     startPage.style = "display: none";
+    canvas.classList.remove('hidden');
+    const game = new Game(ctx);
+    game.start();
+  };
+
+  replayButton.onclick = function () {
+    loosePage.style = "display: none";
     canvas.classList.remove('hidden');
     const game = new Game(ctx);
     game.start();
