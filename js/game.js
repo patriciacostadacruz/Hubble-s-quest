1f7361201b46f2174870d5549dda8dc4a48b6992
patriciacostadacruz <patriciacstcz@gmail.com>
2022-12-03 11:54:42 +0100

    Fixes collision body to body

--- js/game.js
+++ js/game.js
@@ -5,6 +5,7 @@ class Game {
     this.player = new Player(100, 360, 160, 180);
     this.npcs = [];
     this.level = 1;
+    this.generateInterval = undefined;
     // this.gamePaused = false;
   }
 
@@ -33,15 +34,15 @@ class Game {
     this.ctx.clearRect(0, 0, 1000, 600);
   }
 
-  _update() {
+  _update() {    
     this._clean();
     this._writeScoreAndBullets();
     this._displayLevel();
-    this._generateNpcArr();
     this._drawNpcs();
     this._drawPlayer();
     this._drawBullet();
-    this._checkCollisions();
+    this._checkBodyCollision();
+    this._checkBulletCollisions();
     this._levelIncrease();
     this.win();
     window.requestAnimationFrame(() => this._update());
@@ -49,9 +50,9 @@ class Game {
 
   start() {
     this._assignControls();
-    this._update();
     this.player._charge();
-    
+    this._generateNpcArr();
+    this._update();
   }
 
   _drawPlayer() {
@@ -59,23 +60,22 @@ class Game {
   }
 
   _generateNpcArr() {
-    setInterval(() => {
-      if (this.npcs.length < 5) {
-        const newNpc = new Npc(860, 370, 120, 170);
+    this.generateInterval = setInterval(() => {
+      if (this.npcs.length < 100) {
+        const newNpc = new Npc(1100, 370, 120, 170);
         newNpc._assignRole();
         newNpc._assignImage();
+        newNpc._moveLeft();
         this.npcs.push(newNpc);
+        console.log(newNpc);
       }
-    }, 1500);
+    }, 2000);
+    console.log(this.npcs);
   } 
 
   _drawNpcs() {
     this.npcs.forEach((npc) => {
-      this.ctx.drawImage(this.npcs[0].image, this.npcs[0].x, this.npcs[0].y, this.npcs[0].width, this.npcs[0].height);
-      this.npcs[0]._moveLeft();
-      if (this.npcs[0].x < 0 - this.npcs[0].width) {
-        this.npcs.splice(0, 1);
-      }
+      this.ctx.drawImage(npc.image, npc.x, npc.y, npc.width, npc.height);
     });
   }
 
@@ -83,10 +83,10 @@ class Game {
     this.player.bullets.forEach(bullet => {
       if (bullet.isShot) {
       this.ctx.drawImage(bullet.image, bullet.x, bullet.y, bullet.width, bullet.height);
-      if (bullet.x > bullet.x + bullet.width) {
+        if (bullet.x > bullet.x + bullet.width) {
         const bulletIndex = this.player.bullets.indexOf(bullet);
         this.player.bullets.splice(bulletIndex, 1);
-      }
+        }
       }
     });
   }
@@ -98,37 +98,41 @@ class Game {
     this.ctx.fillText(`Bullets: ${this.player.bulletCount}`, 900, 80);   
   }
 
-  _checkCollisions() {
+  _checkBodyCollision() {    
     this.npcs.forEach((npc) => {
+      const npcIndex = this.npcs.indexOf(npc);
       if (this.player.x < npc.x + npc.width && this.player.x + this.player.width > npc.x && this.player.y < npc.y + npc.height && this.player.y + this.player.height > npc.y) {
         if (npc.role === "enemy") {
+          console.log(npc);
           this.points -= 1;
-        }
-      }    
-      // if (this.player.x < this.npcs[0].x + this.npcs[0].width && this.player.x + this.player.width > this.npcs[0].x && this.player.y < this.npcs[0].y + this.npc[0].height && this.player.y + this.player.height > this.npcs[0].y) {
-        // if (this.npcs[0].role === "enemy") {
-          // this.points -= 1;
-        // }
-      // }
-      this.player.bullets.forEach((bullet) => {
-        if (bullet.x < npc.x + npc.width && bullet.x + bullet.width > npc.x && bullet.y < npc.y + npc.height && bullet.y + bullet.height > npc.y) {
-          if (npc.role === "enemy") {
-            this.points += 1;
-            const npcIndex = this.npcs.indexOf(npc);
-            this.npcs.splice(npcIndex, 1);
-            const bulletIndex = this.player.bullets.indexOf(bullet);
-            this.player.bullets.splice(bulletIndex, 1);
-          } else if (npc.role === "friend") {
-            const npcIndex = this.npcs.indexOf(npc);
-            this.npcs.splice(npcIndex, 1);
-            const bulletIndex = this.player.bullets.indexOf(bullet);
-            this.player.bullets.splice(bulletIndex, 1);
+          this.npcs.splice(npcIndex, 1);
+          if (this.points <= 0) {
             this.gameOver();
-          };
+          }
         }
-      });
-    });
+      }
+     });
   }
+  _checkBulletCollisions(){
+    this.npcs.forEach((npc) => {
+      const npcIndex = this.npcs.indexOf(npc);
+          this.player.bullets.forEach((bullet) => {
+            const bulletIndex = this.player.bullets.indexOf(bullet);
+            if (bullet.x < npc.x + npc.width && bullet.x + bullet.width > npc.x && bullet.y < npc.y + npc.height && bullet.y + bullet.height > npc.y) {
+              if (npc.role === "enemy" && bullet.isShot) {
+                this.points += 1;
+                this.npcs.splice(npcIndex, 1);
+                this.player.bullets.splice(bulletIndex, 1);
+              } else if (npc.role === "friend" && bullet.isShot) {
+                this.npcs.splice(npcIndex, 1);
+                this.player.bullets.splice(bulletIndex, 1);
+                this.gameOver();
+              }
+            }
+          });
+        });
+  }
+
 
   _levelIncrease() {
     if (this.points === 6 || this.points === 12) {
@@ -156,12 +160,14 @@ class Game {
       canvas.classList.add('hidden');
       const loosePage = document.getElementById("lose-page");
       loosePage.style = "display: block";
+      clearInterval(this.generateInterval);
     // }, 2000);
   }
 
   win() {
     if (this.points > 18) {
       canvas.classList.add('hidden');
+      clearInterval(this.generateInterval);
       const winPage = document.getElementById("win-page");
       winPage.style = "display: block";
     }
